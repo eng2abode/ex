e533473ae17163d183f6f949eef1d4e0b73ce376
Abdulsalam Hail Al_hakami <97464346+eng2abode@users.noreply.github.com>
2022-03-21 22:02:34 +0300

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,15 @@
+package Lab3;
+
+/**
+ * Created by HP on 06/02/2022.
+ */
+public interface Stack<E> {
+    boolean
+    isEmpty();
+    int size();
+    void push(E element);
+    E pop();
+    E top();
+
+
+}
